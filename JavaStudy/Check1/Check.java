e660170399a673500bf8f5cdb759eba59a5203ad
kyonr <kyonradtennis.0206@gmail.com>
2020-11-02 18:42:40 +0900

    更新

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -9,7 +9,7 @@ public class Check{
         * 問1
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
-        int[] data = {3, 1, 2, 7, 5};
+        int[] data = {3, 1, 2, 7, 5,8,4};
         /*
         * 問2
         * 以下のfor文を完成させなさい
@@ -19,7 +19,7 @@ public class Check{
         }
         System.out.println();
         for (int i = 0; i < /*ここに記述*/data.length; i++) {
-            for (int j = /*ここに記述*/4; j > i; j--) {
+            for (int j = /*ここに記述*/data.length-1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
